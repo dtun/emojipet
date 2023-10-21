062ef232db1dbd96188c244f99d4dafc2b9a3fe6
dtun <dtun@me.com>
2023-10-21 16:28:24 -0700

    Add emoji

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -3,7 +3,7 @@ import { Layout, Text } from '@ui-kitten/components';
 function HomeScreen() {
   return (
     <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category="h1">emojipet</Text>
+      <Text category="h1">em😀jipet</Text>
     </Layout>
   );
 }
