1b636540445f7fdcce200d5d00f5b82696f19682
dtun <dtun@me.com>
2023-10-21 16:54:12 -0700

    Update styles

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,9 +1,17 @@
-import { Layout, Text } from '@ui-kitten/components';
+import { Layout, Text, useTheme } from '@ui-kitten/components';
 
 function HomeScreen() {
+  const theme = useTheme();
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category="h1">em😀jipet</Text>
+    <Layout
+      style={[
+        { flex: 1, justifyContent: 'center', alignItems: 'center' },
+        { backgroundColor: theme['color-primary-default'] },
+      ]}
+    >
+      <Text category="h1" style={{ color: theme['color-primary-100'] }}>
+        em😀jipet
+      </Text>
     </Layout>
   );
 }
