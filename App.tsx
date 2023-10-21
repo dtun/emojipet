7237b8f929839a8ce97791ca8b2511523ca8e17e
dtun <dtun@me.com>
2023-10-21 12:42:14 -0700

    Add failing test

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>emojipet</Text>
+      <Text>emojipet
       <StatusBar style="auto" />
     </View>
   );
